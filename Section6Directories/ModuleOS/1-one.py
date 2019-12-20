bd09de7f67b55016fbdfcfb73e73f52ced07bbd0
Nikos Konstantakis <nikonst@sch.gr>
2019-12-20 14:33:46 +0200

    Section6 Directories V0

--- Section6Directories/ModuleOS/1-one.py
+++ Section6Directories/ModuleOS/1-one.py
@@ -1,9 +1,63 @@
 import os
 
 #os.getcwd()
-print(os.getcwd())
+print("Current Diroctory:", os.getcwd())
 
+#listdir()
 print(os.listdir())
+
+#mkdir
+try:
+    os.mkdir('subDirectory')
+except:
+    print("File Exists")
+
+#rmdir
+os.rmdir('subDirectory')
+
+#makedirs
+try:
+    os.makedirs("sub1/sub2")
+except:
+    print("Couldnt make dirs")
+
+#removedirs
+try:
+    os.removedirs("sub1/sub2")
+except:
+    print("Couldnt make dirs")
+
+##listdir()
+print(os.listdir())
+
+with open("test.txt","w") as f:
+    f.write("Some text")
+
+#rename file
+os.rename("test.txt", "file1.txt")
+os.mkdir('testDir')
+os.rename('testDir', 'testOSDir')
+
+#renames
+os.renames('file1.txt','dir1/dir2/file2.txt')
+
+#scandir
+sd = os.scandir()
+
+for s in sd:
+    print(s)
+    print(s.name)
+    print(s.path)
+    print(s.is_file())
+    print(s.is_dir())
+    print(s.stat())
+    print("")
+#delete af file
+#os.remove("test.txt")
+
+
+
+
 
 #cd in/up
 #  os.chdir(path)
@@ -16,17 +70,17 @@ print(os.listdir())
 https://docs.python.org/3.8/library/os.html?highlight=os#os.getcwd
 
 
-os.listdir
-os.mkdir
-os.makedirs
-os.remove
-os.removedirs
-os.rename
-os.renames
-os.rmdir
-os.scandir
-os.scandir.close()
-os.stat
+os.listdir -
+os.mkdir -
+os.makedirs - 
+os.remove -
+os.removedirs -
+os.rename -
+os.renames -
+os.rmdir - 
+os.scandir -
+os.scandir.close() -
+os.stat -
 
 *** no copy file
 *** no move file
