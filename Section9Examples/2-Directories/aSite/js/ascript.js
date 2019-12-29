6ca9924f671fa49bbc20914e9c7c87cde687ff65
nikonst <nikonst@otenet.gr>
2019-12-29 22:26:28 +0200

    Added files in Examples - Directories folder

---
+++ Section9Examples/2-Directories/aSite/js/ascript.js
@@ -0,0 +1,3 @@
+(function () {
+	document.getElementById("for_ascript").innerHTML = "Nice picture, isn't it!";	
+})();
---
+++ Section9Examples/2-Directories/aSite/js/bscript.js
@@ -0,0 +1,4 @@
+(function () {
+	document.getElementById("for_bscript").innerHTML = "Found at: <a href='https://www.pexels.com/'>pexels.com</a>";	
+})();
+
